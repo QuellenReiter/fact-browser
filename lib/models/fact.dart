0875628e5e131faf47486270dfbf7012a5a6af23
simon <simonsasse97@IMACVONSIMON.local>
2022-03-15 16:14:40 +0100

    Add and remove facts from view

--- lib/models/fact.dart
+++ lib/models/fact.dart
@@ -1,12 +1,75 @@
+
+import 'package:flutter/material.dart';
+
 import '../provider/queries.dart';
+import '../utilities/utilities.dart';
+
+class FactController {
+  late TextEditingController factController;
+  late TextEditingController dateController;
+  late TextEditingController linkController;
+  late TextEditingController authorController;
+  late TextEditingController mediaController;
+  late TextEditingController languageController;
+
+
+  FactController.fromMap(Map<String, dynamic> statement) {
+    factController = TextEditingController(text: statement["text"]);
+    dateController = TextEditingController(text: statement["date"]);
+    linkController =
+        TextEditingController(text: statement["statementLink"]);
+    authorController = TextEditingController(text: statement["author"]);
+    mediaController = TextEditingController(text: statement["medium"]);
+  }
+
+  FactController(Fact fact){
+    factController = TextEditingController(text: fact.factText);
+    // add date conversion functions
+    dateController = TextEditingController(text: fact.factDate);
+    linkController =
+        TextEditingController(text: fact.factLink);
+    authorController = TextEditingController(text: fact.factAuthor);
+    mediaController = TextEditingController(text: fact.factMedia);
+    languageController = TextEditingController(text: fact.factLanguage);
+  }
+
+  void dispose() {
+    factController.dispose();
+    dateController.dispose();
+    linkController.dispose();
+    authorController.dispose();
+    mediaController.dispose();
+  }
+}
+
+class FactControllers{
+  List<FactController> controllers = [];
+
+  void dispose(){
+    for (FactController c in controllers) {
+      c.dispose();
+    }
+  }
+
+  FactControllers(Facts facts){
+    for (Fact fact in facts.facts) {
+      controllers.add(FactController(fact));
+    }
+    //add empty fact controller, if no facts given
+    if (facts.facts.isEmpty){
+      controllers.add(FactController(Fact.empty()));
+    }
+  }
+}
 
 class Fact{
-  final String factText;
-  final String factDate;
-  final String factLanguage;
-  final String factLink;
-  final String factAuthor;
-  final String factMedia;
+  late String factText;
+  late String factDate;
+  late String factLanguage;
+  late String factLink;
+  late String factAuthor;
+  late String factMedia;
+
   Fact(
     this.factText,
     this.factAuthor,
@@ -15,13 +78,22 @@ class Fact{
     this.factLink,
     this.factMedia);
     
-    Fact.fromMap(Map<String, dynamic>? map) : 
-      factText = map?[Queries.factText],
-      factAuthor = map?[Queries.factAuthor],
-      factMedia = map?[Queries.factMedia],
-      factDate = map?[Queries.factDate],
-      factLink = map?[Queries.factLink],
-      factLanguage = map?[Queries.factLanguage];
+  Fact.fromMap(Map<String, dynamic>? map) : 
+    factText = map?[Queries.factText],
+    factAuthor = map?[Queries.factAuthor],
+    factMedia = map?[Queries.factMedia],
+    factDate = Utils.formatDate(map?[Queries.factDate]),
+    factLink = map?[Queries.factLink],
+    factLanguage = map?[Queries.factLanguage];
+
+  Fact.empty(){
+    factText = "";
+    factAuthor = "";
+    factMedia = "";
+    factDate = "";
+    factLink = "";
+    factLanguage = "";
+  }
 }
 
 class Facts{
@@ -31,6 +103,7 @@ class Facts{
       facts.add(Fact.fromMap(fact?["node"]));
     }
   }
-      
-  
+  Facts(){
+    facts.add(Fact.empty());
+  }
 }
