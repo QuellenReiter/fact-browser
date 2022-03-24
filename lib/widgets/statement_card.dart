3ded33e4e29d6bd15affde7b834c6d05fb4faadf
simonsasse <simonsasse97@gmail.com>
2022-03-24 17:36:54 +0100

    redesin search card

--- lib/widgets/statement_card.dart
+++ lib/widgets/statement_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:statementmanager/models/statement.dart';
+import 'package:statementmanager/utilities/utilities.dart';
 
 class StatementCard extends StatelessWidget {
   const StatementCard(
@@ -16,15 +17,53 @@ class StatementCard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Flexible(
-              child: Text(
-                statement.statementText,
-                style: const TextStyle(fontSize: 20.0),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(2),
+                      child: Text(
+                        statement.statementText,
+                        style: const TextStyle(fontSize: 20.0),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(2),
+                      color: statement.statementCorrectness ==
+                              CorrectnessCategory.correct
+                          ? Colors.green
+                          : Colors.red,
+                      child: Text("Einordnung: " +
+                          statement.statementCorrectness +
+                          "."),
+                    )
+                  ],
+                ),
               ),
             ),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(statement.statementAuthor),
-                Text(statement.statementMedia)
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Icon(Icons.person),
+                    ),
+                    Text(statement.statementAuthor),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Icon(Icons.newspaper),
+                    ),
+                    Text(statement.statementMedia),
+                  ],
+                ),
               ],
             ),
           ],
